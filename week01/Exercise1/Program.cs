0eacea5f2c7f14707e0627285ecf75fa3b34a6e3
clarkedav <clarkdav282@gmail.com>
2025-09-11 10:15:13 +0100

    Variables rewritten in camel case

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -6,11 +6,11 @@ class Program
     {
         Console.WriteLine("Hello World! This is the Exercise1 Project.");
         Console.WriteLine("What is your first name?");
-        String firstname = Console.ReadLine();
+        String firstName = Console.ReadLine();
 
         Console.WriteLine("What is your last name?");
-        String lastname = Console.ReadLine();
+        String lastName = Console.ReadLine();
 
-        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");    
+        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}.");    
     }
 }
