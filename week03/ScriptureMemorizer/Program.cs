38c488fe56bbdcee2f37f43673e561bac6c78541
clarkedav <clarkdav282@gmail.com>
2025-09-24 17:23:02 +0100

    changes made

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -4,7 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
 
          Reference reference = new Reference("Proverbs", 3, 5, 6);
         string text = "Trust in the Lord with all thine heart; and lean not unto thine own understanding.";
