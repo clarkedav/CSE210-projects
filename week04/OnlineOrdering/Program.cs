3cb2d86b9f99940e2984eb01c63875fbc694aa39
clarkedav <clarkdav282@gmail.com>
2025-10-02 12:49:46 +0100

    update

--- week04/OnlineOrdering/Program.cs
+++ week04/OnlineOrdering/Program.cs
@@ -9,17 +9,17 @@ class Program
         Customer cust1 = new Customer("Clarke David", addr1);
 
         Order order1 = new Order(cust1);
-        order1.AddProduct(new Product("Xbox", 101, 1200, 1));
-        order1.AddProduct(new Product("Stylus Pen", 102, 25, 2));
+        order1.AddProduct(new Product("Xbox", 101, 2200, 1));
+        order1.AddProduct(new Product("Stylus Pen", 102, 50, 2));
 
         // Second Order
         Address addr2 = new Address("45 Queen St", "Toronto", "ON", "Canada");
         Customer cust2 = new Customer("Peterson Smith", addr2);
 
         Order order2 = new Order(cust2);
-        order2.AddProduct(new Product("Air Pods", 201, 300, 1));
-        order2.AddProduct(new Product("Power Bank", 202, 75, 1));
-        order2.AddProduct(new Product("Laptop", 203, 10, 3));
+        order2.AddProduct(new Product("Air Pods", 201, 58, 1));
+        order2.AddProduct(new Product("Power Bank", 202, 120, 1));
+        order2.AddProduct(new Product("Laptop", 203, 1350, 3));
 
         // Display results
         DisplayOrder(order1);
